22caa2381cee3e95270bf4d4058193e11fba9384
Veronica RG <vero.rg0416@gmail.com>
2022-02-09 10:13:17 -0300

    ✨ Created the principal screens

---
+++ src/Screens/ComingSoon/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Container} from './styles';
+
+const ComingSoon = () => {
+  return <Container />;
+};
+
+export default ComingSoon;
---
+++ src/Screens/Movies/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Container} from './styles';
+
+const Movies = () => {
+  return <Container />;
+};
+
+export default Movies;
