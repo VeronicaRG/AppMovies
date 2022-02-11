14e3f65ce5ac7ee4363c60bc53056385b438d83b
Veronica RG <vero.rg0416@gmail.com>
2022-02-11 11:34:20 -0300

    🚧 Coming Soon Screens

--- src/Screens/ComingSoon/index.js
+++ src/Screens/ComingSoon/index.js
@@ -1,8 +1,54 @@
-import React from 'react';
-import {Container} from './styles';
+import React, {useState} from 'react';
+import {Container, Title, ScrollView} from './styles';
+import {getData} from '../Services/data';
+import MovieReview from '../../Components/MovieReview';
 
 const ComingSoon = () => {
-  return <Container />;
+  const imagePath = 'https://image.tmdb.org/t/p/w500';
+  const [upcoming, setUpcoming] = useState([]);
+
+  function fetchMovies() {
+    getData(setUpcoming, '/movie/upcoming');
+  }
+
+  return (
+    <Container>
+      <Title>COMING SOON</Title>
+      <ScrollView>
+        <MovieReview
+          nameMovie="Discover"
+          review="holancrnmcvkrckmxikmef4kcm"
+          imageMovieReview={upcoming}
+        />
+
+        <MovieReview
+          nameMovie="Discover"
+          review="holancrnmcvkrckmxikmef4kcm"
+          imageMovieReview={upcoming}
+        />
+
+        <MovieReview
+          nameMovie="Discover"
+          review="holancrnmcvkrckmxikmef4kcm"
+          imageMovieReview={upcoming}
+        />
+
+        <MovieReview
+          nameMovie="Discover"
+          review={
+            'holancrnmcvkrckmxikmef4kcm \nholancrnmcvkrckmxikmef4kcm\nholancrnmcvkrckmxikmef4kcm'
+          }
+          imageMovieReview={upcoming}
+        />
+
+        <MovieReview
+          nameMovie="Discover"
+          review="holancrnmcvkrckmxikmef4kcm"
+          imageMovieReview={upcoming}
+        />
+      </ScrollView>
+    </Container>
+  );
 };
 
 export default ComingSoon;
--- src/Screens/ComingSoon/styles.js
+++ src/Screens/ComingSoon/styles.js
@@ -1,6 +1,18 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  padding-top: 40px;
   flex: 1;
-  background: yellow;
+  background: #1c1825;
+`;
+export const ScrollView = styled.ScrollView`
+  width: 100%;
+  height: 320px;
+`;
+
+export const Title = styled.Text`
+  margin: 15px;
+  font-size: 20px;
+  font-weight: bold;
+  color: white;
 `;
