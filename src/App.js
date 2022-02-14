be03e4a36b94274949bd951894339df0c9c99764
Veronica RG <vero.rg0416@gmail.com>
2022-02-14 20:03:30 -0300

    🌐 Implement internationalization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
+import './assets/translation/i18next';
+import {useTranslation} from 'react-i18next';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {AnimatedTabBarNavigator} from 'react-native-animated-nav-tab-bar';
+import {Movie as MovieIcon, TvShow, SearchIcon, Calendar} from './svgs';
 import Movies from './Screens/Movies';
 import Series from './Screens/Series';
 import ComingSoon from './Screens/ComingSoon';
 import Searching from './Screens/Searching';
+import {StatusBar} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+const Tab = AnimatedTabBarNavigator();
 
 export function App() {
-  const Tab = createBottomTabNavigator();
+  const {t} = useTranslation();
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: {backgroundColor: '#212031'},
-        }}>
-        <Tab.Screen name="Movies" component={Movies} />
-        <Tab.Screen name="Series" component={Series} />
-        <Tab.Screen name="ComingSoon" component={ComingSoon} />
-        <Tab.Screen name="Searching" component={Searching} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar barStyle={'light-content'} />
+        <Tab.Navigator
+          tabBarOptions={{
+            activeTintColor: '#fff',
+            inactiveTintColor: '#222222',
+            activeBackgroundColor: '#DE1A3A',
+          }}
+          appearance={{
+            tabBarBackground: '#212031',
+          }}>
+          <Tab.Screen
+            name={t('tabs.movies')}
+            component={Movies}
+            options={{
+              tabBarIcon: ({focused, color, size}) => (
+                <MovieIcon
+                  height={size ? size : 24}
+                  width={size ? size : 24}
+                  color={focused ? color : '#B8B7C3'}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name={t('tabs.series')}
+            component={Series}
+            options={{
+              tabBarIcon: ({focused, color, size}) => (
+                <TvShow
+                  height={size ? size : 24}
+                  width={size ? size : 24}
+                  color={focused ? color : '#B8B7C3'}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name={t('tabs.comingSoon')}
+            component={ComingSoon}
+            options={{
+              tabBarIcon: ({focused, color, size}) => (
+                <Calendar
+                  height={size ? size : 24}
+                  width={size ? size : 24}
+                  color={focused ? color : '#B8B7C3'}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name={t('tabs.search')}
+            component={Searching}
+            options={{
+              tabBarIcon: ({focused, color, size}) => (
+                <SearchIcon
+                  height={size ? size : 24}
+                  width={size ? size : 24}
+                  color={focused ? color : '#B8B7C3'}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 export default App;
