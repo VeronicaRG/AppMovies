070a5e46e907c4cac6665e9fbe063d9113d2e5cd
Veronica RG <vero.rg0416@gmail.com>
2022-02-23 10:28:04 -0300

    ✨ Screen coming soon

--- src/Screens/ComingSoon/index.js
+++ src/Screens/ComingSoon/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import {Container} from './styles';
+import {getData} from '../../Services/data';
+import React, {useState, useEffect} from 'react';
+import {Container, Title, ScrollView} from './styles';
+import MovieReview from '../../Components/MovieReview';
+import {useTranslation} from 'react-i18next';
 
 const ComingSoon = () => {
-  return <Container />;
+  const [upcoming, setUpcoming] = useState([]);
+  const {t} = useTranslation();
+
+  function fetchMovies() {
+    getData(setUpcoming, '/movie/upcoming');
+  }
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <Container>
+      <Title>{t('titles.comingSoon')}</Title>
+      <ScrollView>
+        {upcoming.map(movie => (
+          <MovieReview key={movie.id} movie={movie} />
+        ))}
+      </ScrollView>
+    </Container>
+  );
 };
 
 export default ComingSoon;
