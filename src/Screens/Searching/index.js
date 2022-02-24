980b545a78e8295152534fdbc3006585e5b438b1
Veronica RG <vero.rg0416@gmail.com>
2022-02-24 12:28:53 -0300

    ✨ Screen searching

--- src/Screens/Searching/index.js
+++ src/Screens/Searching/index.js
@@ -1,8 +1,98 @@
-import React from 'react';
-import {Container} from './styles';
-
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useEffect, useRef} from 'react';
+import {useTranslation} from 'react-i18next';
+import ModalMovie from '../../Components/ModalMovie';
+import Movie from '../../Components/Movie';
+import {getData} from '../../Services/data';
+import {SearchIcon} from '../../svgs';
+import {
+  Container,
+  Title,
+  ScrollView,
+  BoxInput,
+  LogoInsideInput,
+  TextInput,
+  Text,
+} from './styles';
 const Searching = () => {
-  return <Container />;
-};
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedData, setSelectedData] = useState();
+  const [trending, setTrending] = useState([]);
+  const [foundMovies, setFoundMovies] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const inputRef = useRef();
+  const {t} = useTranslation();
 
+  function inputFocus() {
+    inputRef.current.focus();
+  }
+
+  function fetchMovies() {
+    getData(setTrending, '/trending/movie/day');
+  }
+  function findMovies() {
+    getData(setFoundMovies, `/search/movie?query=${searchText}`);
+  }
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  useEffect(() => {
+    if (searchText) {
+      findMovies();
+    }
+  }, [searchText]);
+
+  return (
+    <Container>
+      <Title>{t('titles.findYourMovie')}</Title>
+      <BoxInput onPress={inputFocus}>
+        <LogoInsideInput>
+          <SearchIcon height={24} width={24} color={'#B8B7C3'} />
+        </LogoInsideInput>
+        <TextInput
+          ref={inputRef}
+          placeholder={t('inPut.nameOfTheMovie/Serie')}
+          placeholderTextColor="#9C9BA8"
+          value={searchText}
+          onChangeText={setSearchText}
+        />
+      </BoxInput>
+      <ScrollView>
+        {searchText === '' ? (
+          <>
+            <Text>{t('titles.trendingNow')}</Text>
+            {trending.map(movie => (
+              <Movie
+                key={movie.id}
+                modal={item => {
+                  setSelectedData(item);
+                  setModalVisible(true);
+                }}
+                movie={movie}
+              />
+            ))}
+          </>
+        ) : (
+          foundMovies.map(movie => (
+            <Movie
+              modal={item => {
+                setSelectedData(item);
+                setModalVisible(true);
+              }}
+              key={movie.id}
+              movie={movie}
+            />
+          ))
+        )}
+      </ScrollView>
+      <ModalMovie
+        data={selectedData}
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        setData={setSelectedData}
+      />
+    </Container>
+  );
+};
 export default Searching;
--- src/Screens/Searching/styles.js
+++ src/Screens/Searching/styles.js
@@ -1,6 +1,50 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  padding-top: 40px;
   flex: 1;
-  background: blue;
+  background: #1c1825;
 `;
+export const ScrollView = styled.ScrollView`
+  width: 100%;
+`;
+
+export const Title = styled.Text`
+  margin: 15px;
+  font-size: 25px;
+  font-weight: bold;
+  color: white;
+`;
+export const BoxInput = styled.Pressable`
+  width: 92%;
+  height: 50px;
+  border-radius: 7px;
+  background: #212031;
+  border: 0.2px;
+  border-color: grey;
+  margin: 10px;
+  margin-left: 15px;
+  margin-bottom: 20px;
+  flex-direction: row;
+  padding: 10px;
+  align-items: center;
+`;
+
+export const LogoInsideInput = styled.View`
+  width: 25px;
+  height: 25px;
+  border-radius: 15px;
+`;
+
+export const TextInput = styled.TextInput`
+  color: white;
+  margin-left: 5px;
+`;
+
+export const Text = styled.Text`
+  margin: 15px;
+  font-size: 15px;
+  font-weight: 900;
+  color: #717079;
+`;
+export const ViewMovie = styled.View``;
